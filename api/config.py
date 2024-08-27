9dca85d4bcfb793c92e96e325a99f6a52832c203
Will Morris <willmorris103@gmail.com>
2024-08-26 23:51:29 -0500

    PG Admin creds in COnfig

--- api/config.py
+++ api/config.py
@@ -3,12 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-    CHROMA_HOST = os.getenv('CHROMA_HOST', 'localhost')
-    CHROMA_PORT = os.getenv('CHROMA_PORT', 8000)
-    POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
-    POSTGRES_DB = os.getenv('POSTGRES_DB', 'webtracker')
-    POSTGRES_USER = os.getenv('POSTGRES_USER', 'user')
-    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'password')
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    CHROMA_HOST = os.getenv("CHROMA_HOST", "localhost")
+    CHROMA_PORT = os.getenv("CHROMA_PORT", 8000)
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "webtracker")
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
     POSTGRES_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
